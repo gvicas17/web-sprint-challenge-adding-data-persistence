d57dac4b2acf4c3929f52169fd953b877a8f4430
Gabby Vicas <68706544+gvicas17@users.noreply.github.com>
2021-01-21 22:18:34 -0500

    started to set up routers and models

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here
+
+const express = require('express')
+
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
+
+const server = express()
+
+server.use(express.json())
+server.use('/api/project', projectRouter)
+server.use('/api/resource', resourceRouter)
+server.use('/api/task', taskRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to your app' })
+})
+
+module.exports = server
