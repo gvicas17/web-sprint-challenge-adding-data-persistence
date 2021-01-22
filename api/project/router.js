bc3cfdcfe278ede071d99d8ef87775689e0f1571
Gabby Vicas <68706544+gvicas17@users.noreply.github.com>
2021-01-22 11:22:15 -0500

    created findAll for projects

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,19 @@
 // build your `/api/projects` router here
+
+const express = require('express')
+const router = express.Router()
+
+const Project = require('./model')
+
+router.get('/', (req, res) => {
+    
+    Project.findAll()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(() => {
+        res.status(500).json({message: 'could not find projects'})
+    })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
 // build your server here
+
+const express = require('express')
+
+const server = express()
+const projectRouter = require('./project/router')
+
+server.use(express.json())
+server.use('/api/projects', projectRouter)
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: 'welcome to your sprint challenge'})
+})
+
+module.exports = server
