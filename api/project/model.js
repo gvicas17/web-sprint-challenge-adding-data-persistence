b3b2459cdcfc39d75a26f60c82d733dc10fa7a19
Gabby Vicas <68706544+gvicas17@users.noreply.github.com>
2021-01-21 23:37:35 -0500

    added table

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,10 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+module.exports = {
+   findAll
+}
+
+function findAll(){
+    return db('projects')
+}
--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
 // build your server here
+
+const express = require('express')
+
+const projectRouter = require('./project/router')
+
+const server = express()
+
+server.use(express.json())
+server.use('/api/projects', projectRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to your app' })
+})
+
+module.exports = server
